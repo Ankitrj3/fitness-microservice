29a3322d66708d9ec8cb5c66ac627cfc5c9c9590
ankitrj3 <ankitrobinranjan@gmail.com>
2025-09-02 22:09:15 +0530

    Add Eureka service and update project configurations
    - Introduced a new Eureka service module with necessary configurations and dependencies.
    - Updated `pom.xml` files for `activityservice` and `userservice` to include Eureka client dependencies.
    - Modified `application.properties` files for both services to configure Eureka client settings.
    - Added encoding settings for the new Eureka module in the IDE configuration files.
    - Created necessary Maven wrapper files for the Eureka module.
    - Added `.gitignore` and `.gitattributes` files for the Eureka module to manage version control settings.
    - Implemented basic application structure for the Eureka service with a main application class and test cases.

--- userservice/src/main/java/com/fitness/userservice/UserserviceApplication.java
+++ userservice/src/main/java/com/fitness/userservice/UserserviceApplication.java
@@ -2,6 +2,7 @@ package com.fitness.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class UserserviceApplication {
